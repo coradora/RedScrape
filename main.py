54dcf353ffec8e4665a1e6a55099c5611c5c8a81
Cora <corradora30@gmail.com>
2021-02-12 09:48:58 -0500

    basic shell functionality

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ reddit = praw.Reddit(
     client_secret="redacted",
     user_agent="Scrape"
 )
+
+inputSub = input()
+if inputSub == '' or ' ':
+    inputSub = 'all'
+sub = reddit.subreddit(inputSub)
+
+postDict = {
+    "title": [], "url": [], "body": []
+}
+
+for post in sub.hot(limit=10):
+    postDict["title"].append(post.title)
+    postDict["url"].append(post.url)
+    postDict["body"].append(post.selftext)
+
+postData = pand.DataFrame(postDict)
+print(postData)
+postData.to_csv('test.csv', index=False)
